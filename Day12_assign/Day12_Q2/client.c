193e8e171c294184cfe26852c742c0f47059146e
poonamkokitkar <poonamkokitkar19@gmail.com>
2024-05-18 15:55:17 +0530

    Changed code of Q2

--- Day12_assign/Day12_Q2/client.c
+++ Day12_assign/Day12_Q2/client.c
@@ -1,47 +1,53 @@
-#include<arpa/inet.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
+#include <arpa/inet.h>
 #include <sys/un.h>
-
-#define SOCKET_PATH "/home/poonam/Desktop/My repositories/EOS_Assignments/Day12_assign/Day12_Q2/desd_socket"
-
 // client
 
-int main(){
+#define SOCKET_PATH "/home/poonam/Desktop/12_pra/desd_client"
 
-int cli_fd, ret, n1, n2;
-int res;
+int main() {
+	int cli_fd, ret,sum, num1, num2;
+	struct sockaddr_un serv_addr;
+	char sun_path[102];
+	cli_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+	if(cli_fd < 0) {
+		perror("socket() failed");
+		_exit(1);
+	}
 
-struct sockaddr_un serv_addr;
-
-cli_fd = socket(AF_UNIX, SOCK_STREAM, 0);
-if(cli_fd < 0){
-       perror("socket() failed");
-	   _exit(1);
-}
-
-serv_addr.sun_family = AF_UNIX;
-strcpy(serv_addr.sun_path, SOCKET_PATH);
-
-ret = connect(cli_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-if(ret < 0) {
+	serv_addr.sun_family = AF_UNIX;
+	strcpy(serv_addr.sun_path, SOCKET_PATH);
+	//serv_addr.sin_port = htons(SERV_PORT);
+//	inet_aton(SERV_IP, &serv_addr.sin_addr);
+	ret = connect(cli_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+	if(ret < 0) {
 		perror("connect() failed");
 		_exit(2);
 	}
+		printf("enter two numbers:\n");
+		scanf("%d %d",&num1,&num2);
+		write(cli_fd, &num1, sizeof(num1));
+		write(cli_fd, &num2, sizeof(num2));
 
-printf("client: \n");
-printf("Enter two no.s: \n");
-scanf("%d %d",&n1, &n2);
 
-write(cli_fd, &n1, sizeof(n1));
-write(cli_fd, &n2, sizeof(n2));
+	    read(cli_fd, &sum, sizeof(sum));
+		printf("the addition is %d\n",sum);
 
-read(cli_fd, &res, sizeof(res));
-printf("Addition: %d \n",res);
+/*
+	do {
+		printf("client: ");
+		gets(str);
+		write(cli_fd, str, strlen(str)+1);
 
-return 0;
+		read(cli_fd, str, sizeof(str));
+		printf("server: %s\n", str);
+	}while(strcmp(str, "bye")!=0);
+*/
+	close(cli_fd);
+	return 0;
 }
--- Day12_assign/Day12_Q2/server.c
+++ Day12_assign/Day12_Q2/server.c
@@ -1,62 +1,60 @@
-#include<stdio.h>
-#include<string.h>
-#include<sys/un.h>
-#include<unistd.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<netinet/ip.h>
-#include<arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
 
-#define SOCKET_PATH "/home/poonam/Desktop/My repositories/EOS_Assignments/Day12_assign/Day12_Q2/desd_socket"
+#define SOCKET_PATH "/home/poonam/Desktop/12_pra/desd_client"
 
 int main(){
-
-int serv_fd, ret, n1, n2;
-int res, cli_fd, cli_addr;
-socklen_t socklen;
-char sun_path[108];
-
-struct sockaddr_un serv_addr;
-// ...
-serv_fd = socket(AF_UNIX, SOCK_STREAM, 0);
-	if(serv_fd < 0) {
+	int serv_fd,sum,num1,num2, cli_fd, ret;
+	struct sockaddr_un serv_addr, cli_addr;
+	socklen_t socklen;
+	char sun_path[102];
+	serv_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+	if(serv_fd < 0){
 		perror("socket() failed");
-		_exit(1);
+		_exit(0);
 	}
 
-serv_addr.sun_family = AF_UNIX;
-//serv_addr.sun_path = "/home/user/Desktop/ClassWork/EOS_Assignments/Day12_Q2/desd_socket";
-strcpy(serv_addr.sun_path, SOCKET_PATH);
-
-ret = bind(serv_fd, (struct sockaddr*) &serv_addr, sizeof(serv_addr));
-if(ret < 0) {
+	serv_addr.sun_family = AF_UNIX;
+	strcpy(serv_addr.sun_path, SOCKET_PATH);
+	
+//	inet_aton(SERV_IP, &serv_addr.sun_addr);
+	ret = bind(serv_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+	if(ret < 0) {
 		perror("bind() failed");
 		_exit(2);
 	}
 
 	listen(serv_fd, 5);
-
+while(1){
 	socklen = sizeof(cli_addr);
+	cli_fd = accept(serv_fd, (struct sockaddr*)&cli_addr, &socklen);
+	if(cli_fd < 0) {
+		perror("accept() failed");
+		_exit(3);
+	}
+		read(cli_fd, &num1, sizeof(num1));
+		read(cli_fd, &num2, sizeof(num2));
+		sum = num1 + num2;
+		write(cli_fd, &sum, sizeof(sum));
+	}
+/*
+	do {
+		read(cli_fd, str, sizeof(str));
+		printf("client: %s\n", str);
 
-while(1) {
-cli_fd = accept(serv_fd, (struct sockaddr*)&cli_addr, &socklen);
-if(cli_fd < 0){
-      perror("accept() failed");
-	  _exit(3);
-}
+		printf("server: ");
+		gets(str);
+		write(cli_fd, str, strlen(str)+1);
+	}while(strcmp(str, "bye")!=0);
+*/
+	close(cli_fd);
 
-read(cli_fd, &n1, sizeof(n1));
-read(cli_fd, &n2, sizeof(n2));
-printf("......Numbers received......\n");
-
-res = n1 + n2;
-write(cli_fd, &res, sizeof(res));
-close(cli_fd);
-}
-
-//close(cli_fd);
-shutdown(serv_fd, SHUT_RDWR);
-
-
-return 0;
+	shutdown(serv_fd, SHUT_RDWR);
+	return 0;
 }
